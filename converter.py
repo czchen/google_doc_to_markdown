6a3caf54f948426e4375053537f32f36d20810b9
ChangZhuo Chen <czchen@gmail.com>
2013-02-23 00:43:21 +0800

    Implement indent

--- converter.py
+++ converter.py
@@ -6,19 +6,18 @@ import sys
 from html import parser
 
 class HTMLParser(parser.HTMLParser):
-    _INDENT = 3
+    _OLLI_INDENT = 3
     _CODE_INDENT = 4
+
+    _DEFAULT_STATE = {
+            "name": "start",
+            "indent": 0,
+    }
 
     def __init__(self, output, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._state = [self._DEFAULT_STATE]
         self._output = output
-        self._state = ["start"]
-        self._indent = 0
-        self._attrs = None
-
-    @staticmethod
-    def _escape(data):
-        return data
 
     def _get_pop_state_endtag(tag, **kwargs):
         def _func(self, tag_):
@@ -33,33 +32,53 @@ class HTMLParser(parser.HTMLParser):
     def _null_func(self, *args, **kwargs):
         pass
 
+    def _get_current_state(self):
+        return self._state[-1]
+
+    def _get_current_state_name(self):
+        return self._get_current_state()["name"]
+
+    def _get_current_indent(self):
+        return self._get_current_state()["indent"]
+
     def _write_data(self, data):
-        self._output.write(self._escape(data))
+        data = re.sub(r"([\\*_{}\[\]()#+-\.!])", r"\1", data)
+        self._output.write(data)
 
     def handle_starttag(self, tag, attrs):
-        getattr(self, "_{}_handle_starttag".format(self._state[-1]), HTMLParser._null_func)(tag, attrs)
+        getattr(self, "_{}_handle_starttag".format(self._get_current_state_name()), HTMLParser._null_func)(tag, attrs)
 
     def handle_endtag(self, tag):
-        getattr(self, "_{}_handle_endtag".format(self._state[-1]), HTMLParser._null_func)(tag)
+        getattr(self, "_{}_handle_endtag".format(self._get_current_state_name()), HTMLParser._null_func)(tag)
 
     def handle_data(self, data):
-        getattr(self, "_{}_handle_data".format(self._state[-1]), HTMLParser._null_func)(data)
+        getattr(self, "_{}_handle_data".format(self._get_current_state_name()), HTMLParser._null_func)(data)
 
     # start
     def _start_handle_starttag(self, tag, attrs):
         if tag == "body":
-            self._state.append("body")
+            self._state.append({
+                "name": "body",
+                "indent": 0,
+            })
 
     # body
     def _body_handle_starttag(self, tag, attrs):
         if tag == "ol":
-            self._state.append("ol")
+            self._state.append({
+                "name": "ol",
+                "indent": self._get_current_indent(),
+            })
+            return
 
         match = re.match("h(\d+)", tag)
         if match:
             level = int(match.group(1))
-            self._output.write(" " * self._indent + "#" * level + " ")
-            self._state.append("h")
+            self._output.write("#" * level + " ")
+            self._state.append({
+                "name": "h",
+                "indent": self._get_current_indent()
+            })
             return
 
     _body_handle_endtag = _get_pop_state_endtag("body")
@@ -76,8 +95,11 @@ class HTMLParser(parser.HTMLParser):
     # ol
     def _ol_handle_starttag(self, tag, attrs):
         if tag == "li":
-            self._output.write(" " * self._indent + "* ")
-            self._state.append("olli")
+            self._output.write(" " * self._get_current_indent() + "* ")
+            self._state.append({
+                "name": "olli",
+                "indent": self._get_current_indent() + self._OLLI_INDENT,
+            })
             return
 
     _ol_handle_endtag = _get_pop_state_endtag("ol", enter=1)
@@ -85,8 +107,11 @@ class HTMLParser(parser.HTMLParser):
     # olli
     def _olli_handle_starttag(self, tag, attrs):
         if tag == "a":
-            self._attrs = attrs
-            self._state.append("a")
+            self._state.append({
+                "name": "a",
+                "indent": self._get_current_indent(),
+                "attrs": attrs,
+            })
             return
 
     _olli_handle_data = _write_data
@@ -96,7 +121,7 @@ class HTMLParser(parser.HTMLParser):
     # a
     def _a_handle_data(self, data):
         href = ""
-        for key, value in self._attrs:
+        for key, value in self._get_current_state()["attrs"]:
             if key == "href":
                 href = value
                 break
